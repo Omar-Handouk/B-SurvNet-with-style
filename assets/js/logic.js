3c54c0b99681e1944aacd7b038198e4ba0bed899
HussienAbdelaal <hussienabdelaal105@gmail.com>
2018-10-24 23:35:56 +0200

    updated logic.js file

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,1 +1,23 @@
-console.log("logic test!!");
+$(document).ready(function () {
+    const SupplyUrl = 'http://vehicle-manufacture-rest-secured3-anxious-hedgehog.mybluemix.net/api/Supply';
+    const ReceiverUrl = 'http://vehicle-manufacture-rest-secured3-anxious-hedgehog.mybluemix.net/api/Receiver';
+    $('.btn').click(function () {
+        $.ajax({
+            url: SupplyUrl,
+            type: "GET",
+            success: function (data) {
+                $.ajax({
+                    url: ReceiverUrl,
+                    type: "GET",
+                    success: function(data){
+                        console.log(data)
+                    }
+                })
+            },
+            error: function (error) {
+                console.log(error)
+            }
+        })
+    })
+
+})
