2935828d12707937bb687996ccd30cd6b97e8f21
HussienAbdelaal <hussienabdelaal105@gmail.com>
2018-10-25 02:56:51 +0200

    modified logic.js and index.html

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,18 +1,19 @@
 $(document).ready(function () {
     const SupplyUrl = 'http://vehicle-manufacture-rest-secured3-anxious-hedgehog.mybluemix.net/api/Supply';
-    const ReceiverUrl = 'http://vehicle-manufacture-rest-secured3-anxious-hedgehog.mybluemix.net/api/Receiver';
-    $('.btn').click(function () {
+    const ReceiverUrl = 'http://vehicle-manufacture-rest-secured3-anxious-hedgehog.mybluemix.net/api/Disaster';
+    $('.btn-report').click(function () {
         $.ajax({
             url: SupplyUrl,
             type: "GET",
             success: function (data) {
-                $.ajax({
-                    url: ReceiverUrl,
-                    type: "GET",
-                    success: function(data){
-                        console.log(data)
-                    }
-                })
+                console.log(data)
+                // $.ajax({
+                //     url: ReceiverUrl,
+                //     type: "GET",
+                //     success: function(data){
+                //         console.log(data)
+                //     }
+                // })
             },
             error: function (error) {
                 console.log(error)
